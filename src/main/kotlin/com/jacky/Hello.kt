02add883fa804066180b129193fe3b4af94e6176
thhsiehman <thhsiehman@gmail.com>
2019-02-26 00:20:44 +0800

    test

---
+++ src/main/kotlin/com/jacky/Hello.kt
@@ -0,0 +1,14 @@
+package com.jacky
+
+fun main(args: Array<String>) {
+   // println("Hello Kotlin");
+    val h = Human();
+    h.hello();
+}
+
+
+class Human{
+    fun hello(){
+        println("Hello jacky");
+    }
+}
